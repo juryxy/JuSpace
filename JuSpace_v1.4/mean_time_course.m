c4a163d332fe43ffdd4c28e3f56e02592ec05722
juryxy <54351124+juryxy@users.noreply.github.com>
2022-05-29 12:06:08 +0200

    Add files via upload

---
+++ JuSpace_v1.4/mean_time_course.m
@@ -0,0 +1,43 @@
+function [D,Reg_all] = mean_time_course(data_mean, data_ROI, numberROIs)
+%[D,Reg_all] = mean_time_course(data_for_mean, mask_ROIs,numberROIs)
+
+
+size_data = size(data_mean);
+
+[ROI_matrix] = resize_img_useTemp_imcalc(data_ROI,data_mean{1});
+vectorROI = reshape(ROI_matrix,size(ROI_matrix,1)*size(ROI_matrix,2)*size(ROI_matrix,3),1);
+
+D = zeros(size_data(1,1),length(numberROIs));
+Reg_all = zeros(length(numberROIs),1);
+% mean time course extraction
+
+for i =1:size_data(1,1)
+    try
+        
+    disp(['Extracting data for ' data_mean{i}]);
+  
+        X = spm_vol(data_mean{i});
+        Y = spm_read_vols(X);
+        
+        if size(ROI_matrix)~=size(Y)
+            [ROI_matrix] = resize_img_useTemp_imcalc(data_ROI,data_mean{i});
+            vectorROI = reshape(ROI_matrix,size(ROI_matrix,1)*size(ROI_matrix,2)*size(ROI_matrix,3),1);
+        end
+        vector_vol = reshape(Y,size(Y,1)*size(Y,2)*size(Y,3),1);
+        if numel(vectorROI)==numel(vector_vol)
+            for j = 1:length(numberROIs)
+                Reg_j = vector_vol(round(vectorROI) == numberROIs(j));
+                Reg_all(j) = length(Reg_j);
+                D(i,j)= mean(removenan_my(Reg_j,':'));
+                %disp([num2str(length(Reg_j)) ' voxels found for file ' num2str(i)  ' region ID ' num2str(numberROIs(j)) ': ' num2str(D(i,j))]);
+            end
+        else
+            disp(['Wrong dimensions: ' data_mean{i}]);
+        end
+    catch
+    disp(['Failed: ' data_mean{i}]);
+    end
+end
+
+
+
---
+++ JuSpace_v1.4/resize_img_useTemp_imcalc.m
@@ -0,0 +1,33 @@
+function [img3d] = resize_img_useTemp_imcalc(file,temp)
+% function [img3d] = resize_img_useTemp_imcalc(file,temp,opt_save)
+%-----------------------------------------------------------------------
+% Job saved on 15-Jul-2015 09:33:17 by cfg_util (rev $Rev: 6134 $)
+% spm SPM - SPM12 (6225)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+
+
+[path] = fileparts(file);
+
+file_temp = char(append_prefix_to_fileNames_my(file,'rmyTemp'));
+matlabbatch{1}.spm.util.imcalc.input = {
+                                        temp
+                                        file
+                                        };
+matlabbatch{1}.spm.util.imcalc.output = file_temp;
+matlabbatch{1}.spm.util.imcalc.outdir = {path};
+matlabbatch{1}.spm.util.imcalc.expression = 'i2';
+matlabbatch{1}.spm.util.imcalc.var = struct('name', {}, 'value', {});
+matlabbatch{1}.spm.util.imcalc.options.dmtx = 0;
+matlabbatch{1}.spm.util.imcalc.options.mask = 0;
+matlabbatch{1}.spm.util.imcalc.options.interp = 0;
+matlabbatch{1}.spm.util.imcalc.options.dtype = 4;
+spm_jobman('run',matlabbatch);
+
+img3d = spm_read_vols(spm_vol(file_temp));
+% if opt_save == 1
+% else
+    
+file_temp = regexprep(file_temp,',1','');
+delete(file_temp);
+% end
